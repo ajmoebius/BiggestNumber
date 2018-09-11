fb69e486b680786afa6607aa1410f318121cbaff
ajmoebius <ajmoebius@ecc-jaguar.csuchico.edu>
2018-09-10 17:04:00 -0700

    added conditionl statements, input requests, and output biggest number

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -4,19 +4,29 @@
 int main()
 {
   //declare variables here
-
+  int one;
+  int two;
+  int thr;
   std::cout<<"Please enter a whole number:\n";
   //get first input using std::cin>>
+  std::cin>>one;
 
   std::cout<<"Please enter another whole number:\n";
   //get second input using std::cin>> again with a second variable
+  std::cin>>two;
 
   //determine if first or second is bigger and store that in a third variable
-
-
+  if(one<two)
+   {
+    thr=two;
+   }
+  else
+  {
+   thr=one;
+  }
   std::cout<<"Of those two numbers, the biggest is: ";
   //print out the variable representing the biggest number
-
+  std::cout<<thr<<std::endl;
   std::cout<<std::endl<<"Thank you for playing.\n";
   
   return 0;
